b01b25cd2cb8a4ebaea6ff334ad5db5eade3ef5a
Slava.P <p4slava@gmail.com>
2017-01-24 18:22:11 -0800

    remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,11 @@ namespace po = boost::program_options;
 // This is just for the example so far...
 extern int camera_should_move2();
 
-class MyObject{};
-
 int main(int argc, char** argv) {
 	po::options_description desc("Allowed options");
 	desc.add_options()
 	    ("help,h", "print usage message")
 	    ("debug", po::value<bool>()->default_value(false), "enable debug log level")
-        ("source", po::value<bool>()->default_value(false), "source video file for testing");
 	;
 
 	po::variables_map vm;
